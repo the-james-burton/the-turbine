6a3305ab062e24ec8838cfada8670973f22008b2
the-james-burton <the.james.burton@gmail.com>
2015-05-11 17:37:20 +0100

    added missing files

---
+++ turbine/src/main/java/org/jimsey/projects/turbine/spring/camel/processors/InstrumentProcessor.java
@@ -0,0 +1,48 @@
+/**
+ * The MIT License
+ * Copyright (c) 2015 the-james-burton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jimsey.projects.turbine.spring.camel.processors;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.jimsey.projects.turbine.spring.TurbineConstants;
+import org.jimsey.projects.turbine.spring.domain.Instrument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InstrumentProcessor implements Processor {
+
+  private static final Logger logger = LoggerFactory.getLogger(InstrumentProcessor.class);
+
+  @Override
+  public void process(final Exchange exchange) throws Exception {
+    Message message = exchange.getIn();
+    Instrument instrument = message.getMandatoryBody(Instrument.class);
+    String type = message.getHeader(TurbineConstants.HEADER_FOR_OBJECT_TYPE, String.class);
+   
+    logger.info("consumed: [{}={}, quoteId={}]",TurbineConstants.HEADER_FOR_OBJECT_TYPE, type, instrument.getId());
+  }
+
+}
