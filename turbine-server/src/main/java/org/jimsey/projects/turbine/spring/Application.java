997f97118230d777569485ce8ee4c26a142ea0fb
the-james-burton <the.james.burton@gmail.com>
2015-07-28 18:02:21 +0100

    springfox/swagger api autogen docs

--- turbine-server/src/main/java/org/jimsey/projects/turbine/spring/Application.java
+++ turbine-server/src/main/java/org/jimsey/projects/turbine/spring/Application.java
@@ -23,23 +23,49 @@
 package org.jimsey.projects.turbine.spring;
 
 import java.util.Arrays;
+import java.util.List;
 
 import javax.validation.constraints.NotNull;
 
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.jimsey.projects.turbine.spring.service.Ping;
+import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.WildcardType;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import com.fasterxml.classmate.TypeResolver;
+import static com.google.common.collect.Lists.*;
+import static springfox.documentation.schema.AlternateTypeRules.*;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 @EnableElasticsearchRepositories(basePackages = "org.jimsey.projects.turbine.spring.elasticsearch.repositories")
 public class Application extends CamelConfiguration {
 
@@ -54,16 +80,77 @@ public class Application extends CamelConfiguration {
         Application.class, args);
 
     // logBeanNames(spring);
-
     Ping ping = (Ping) spring.getBean("ping");
     long message = ping.ping();
     logger.info(String.format("ping=%s", message));
-
-    // ObjectMapper objectMapper = new ObjectMapper();
-    // objectMapper.writeValue(resultFile, value);
-
   }
 
+  @Bean
+  public Docket petApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+          .apis(RequestHandlerSelectors.any())
+          .paths(PathSelectors.any())
+          .build()
+        .pathMapping("/")
+        .directModelSubstitute(LocalDate.class,
+            String.class)
+        .genericModelSubstitutes(ResponseEntity.class)
+        .alternateTypeRules(
+            newRule(typeResolver.resolve(DeferredResult.class,
+                    typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                typeResolver.resolve(WildcardType.class)))
+        .useDefaultResponseMessages(false)
+        .globalResponseMessage(RequestMethod.GET,
+            newArrayList(new ResponseMessageBuilder()
+                .code(500)
+                .message("500 message")
+                .responseModel(new ModelRef("Error"))
+                .build()))
+        .securitySchemes(newArrayList(apiKey()))
+        .securityContexts(newArrayList(securityContext()))
+        .enableUrlTemplating(true)
+        ;
+  }
+
+  @Autowired
+  private TypeResolver typeResolver;
+
+  private ApiKey apiKey() {
+    return new ApiKey("mykey", "api_key", "header");
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+        .securityReferences(defaultAuth())
+        .forPaths(PathSelectors.regex("/anyPath.*"))
+        .build();
+  }
+
+  List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope
+        = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return newArrayList(
+        new SecurityReference("mykey", authorizationScopes));
+  }
+
+  @Bean
+  SecurityConfiguration security() {
+    return new SecurityConfiguration(
+        "test-app-client-id",
+        "test-app-realm",
+        "test-app",
+        "apiKey");
+  }
+
+  @Bean
+  UiConfiguration uiConfig() {
+    return new UiConfiguration(
+        "validatorUrl");
+  }
+  
   @SuppressWarnings("unused")
   private static void logBeanNames(final ConfigurableApplicationContext spring) {
     logger.info("Let's inspect the beans provided by Spring Boot:");
