453561eb76e413dcc87b92ddeaaf94c5b13c62ac
the-james-burton <the.james.burton@gmail.com>
2015-08-05 17:46:52 +0100

    added CORS filter

---
+++ turbine-server/src/main/java/org/jimsey/projects/turbine/spring/web/SimpleCORSFilter.java
@@ -0,0 +1,36 @@
+package org.jimsey.projects.turbine.spring.web;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * TODO replace this with first-class CORS support in Spring 4.2GA when available in Spring boot
+ * https://spring.io/blog/2015/06/08/cors-support-in-spring-framework
+ * @see <pre>CrossOrigin</pre>
+ * 
+ * @author jimsey
+ */
+@Deprecated
+@Component
+public class SimpleCORSFilter implements Filter {
+
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		chain.doFilter(req, res);
+	}
+
+	public void init(FilterConfig filterConfig) {}
+
+	public void destroy() {}
+
+}
