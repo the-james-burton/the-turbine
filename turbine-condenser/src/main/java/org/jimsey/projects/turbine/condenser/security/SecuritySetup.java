499947f7a9bf5ba97e87f81074298e2048908840
the-james-burton <the.james.burton@gmail.com>
2016-04-20 08:00:15 +0100

    separate class for cors filter, directory for security

---
+++ turbine-condenser/src/main/java/org/jimsey/projects/turbine/condenser/security/SecuritySetup.java
@@ -0,0 +1,68 @@
+/**
+ * The MIT License
+ * Copyright (c) 2015 the-james-burton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jimsey.projects.turbine.condenser.security;
+
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecuritySetup extends WebSecurityConfigurerAdapter {
+
+  private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    CorsFilter corsFilter = TurbineCorsFilter.newCorsFilter();
+
+    http
+        // http://stackoverflow.com/questions/31724994/spring-data-rest-and-cors
+        .addFilterBefore(corsFilter, ChannelProcessingFilter.class)
+        // .csrf().disable()
+        .antMatcher("/turbine/**")
+        .authorizeRequests()
+        // .anyRequest()
+        // .hasAnyRole("USER")
+        // .and()
+        // .httpBasic();
+        .anyRequest()
+        .permitAll();
+  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+        .inMemoryAuthentication()
+        .withUser("user").password("password").roles("USER");
+  }
+}
--- turbine-condenser/src/main/java/org/jimsey/projects/turbine/condenser/security/TurbineCorsFilter.java
+++ turbine-condenser/src/main/java/org/jimsey/projects/turbine/condenser/security/TurbineCorsFilter.java
@@ -1,52 +1,40 @@
-package org.jimsey.projects.turbine.condenser;
+/**
+ * The MIT License
+ * Copyright (c) 2015 the-james-burton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jimsey.projects.turbine.condenser.security;
 
-import java.lang.invoke.MethodHandles;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@Configuration
-@EnableWebSecurity
-public class SecuritySetup extends WebSecurityConfigurerAdapter {
-
-  private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-        // http://stackoverflow.com/questions/31724994/spring-data-rest-and-cors
-        .addFilterBefore(this.corsFilter(), ChannelProcessingFilter.class)
-        .csrf().disable()
-        .antMatcher("/turbine/**")
-        .authorizeRequests()
-        .anyRequest()
-        .permitAll();
-    // .anyRequest()
-    // .hasAnyRole("USER")
-    // .and()
-    // .httpBasic();
-  }
+public class TurbineCorsFilter {
 
   /*
    * With Spring Security, automatic registration is still expected
-   * by spring Boot but DOES NOT WORK
+   * by spring Boot when annotated with @Bean but it DOES NOT WORK
    * Instead, this filter is registered in the configure() method above
    * http://stackoverflow.com/questions/31724994/spring-data-rest-and-cors
    */
-  @Bean
-  public CorsFilter corsFilter() {
-    logger.info("corsFilterDirect init...");
+  public static CorsFilter newCorsFilter() {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true); // you USUALLY want this
@@ -112,10 +100,4 @@ public class SecuritySetup extends WebSecurityConfigurerAdapter {
   // };
   // }
 
-  @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    auth
-        .inMemoryAuthentication()
-        .withUser("user").password("password").roles("USER");
-  }
 }
