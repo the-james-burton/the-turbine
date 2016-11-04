07da1b68ccd4bf2f7ef0adfdbefa2f051e9c6537
the-james-burton <the.james.burton@gmail.com>
2016-11-04 18:25:06 +0000

    issue #12 added simple test for TurbineMatchers

---
+++ turbine-inspector/src/test/java/org/jimsey/projects/turbine/inspector/matchers/TurbineMatchersTest.java
@@ -0,0 +1,29 @@
+package org.jimsey.projects.turbine.inspector.matchers;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.jimsey.projects.turbine.inspector.matchers.TurbineMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TurbineMatchersTest {
+
+  @Test
+  public void testIsNumeric() {
+    assertThat("1", isNumeric());
+    assertThat("1.2", isNumeric());
+    assertThat("100000.10000", isNumeric());
+    assertThat("0.00001", isNumeric());
+    
+    assertThat("1 a", not(isNumeric()));
+    assertThat("one", not(isNumeric()));
+    assertThat(".", not(isNumeric()));
+    assertThat("", not(isNumeric()));
+    assertThat(null, not(isNumeric()));
+    
+    // interesting result...
+    assertThat("001.2", not(isNumeric()));
+
+  }
+  
+}
