a63cf5be743091ac5d4df66279c974b254b7757a
the-james-burton <the.james.burton@gmail.com>
2017-05-22 19:44:05 +0100

    issue #6 added FisherTransform indicator

---
+++ turbine-engine-hall/turbine-condenser/src/main/java/org/jimsey/projects/turbine/condenser/domain/indicators/FisherTransform.java
@@ -0,0 +1,55 @@
+/**
+ * The MIT License
+ * Copyright (c) ${project.inceptionYear} the-james-burton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jimsey.projects.turbine.condenser.domain.indicators;
+
+import java.lang.invoke.MethodHandles;
+import java.util.HashMap;
+import java.util.Map;
+
+import eu.verdelhan.ta4j.TimeSeries;
+import eu.verdelhan.ta4j.indicators.oscillators.FisherIndicator;
+import eu.verdelhan.ta4j.indicators.simple.ClosePriceIndicator;
+
+/**
+ * @author the-james-burton
+ */
+@EnableTurbineIndicator(name = "FisherTransform", isOverlay = false)
+public class FisherTransform extends BaseIndicator {
+
+  private final FisherIndicator fisherIndicator;
+
+  public FisherTransform(final TimeSeries series, final ClosePriceIndicator indicator) {
+    super(20, series, MethodHandles.lookup().lookupClass().getSimpleName(), indicator);
+
+    // setup this indicator...
+    fisherIndicator = new FisherIndicator(indicator, timeFrame);
+  }
+
+  @Override
+  public Map<String, Double> computeValues() {
+    Map<String, Double> values = new HashMap<>();
+    values.put("fisherIndicator", fisherIndicator.getValue(series.getEnd()).toDouble());
+    return values;
+  }
+
+}
